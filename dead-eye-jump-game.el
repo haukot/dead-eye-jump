fe1ab41249263936ce5844c5a18553f074609e1e
Dmitry Davydov <haudvd@gmail.com>
2024-05-12 02:48:32 +0700

    renaming

--- dead-eye-jump-game.el
+++ dead-eye-jump-game.el
@@ -1,169 +1,151 @@
-;;; -*- lexical-binding: t -*-
+;;; dead-eye-jump.el --- Hit the target, get the points. -*- lexical-binding: t -*-
+;;; Code:
 
-(defvar target-game-buffer "*Target-Game*")
+;;* Customization
+(defvar dead-eye-jump-game-buffer "*Dead-Eye-Jump-Game*")
 
-(defface target-face-center
-  '((t :background "#aa00aa"))
+(defface dead-eye-jump-game-target-face-center '((t :background "#aa00aa"))
   "Face for the center of the target.")
-(defface target-face-middle
-  '((t :background "yellow"))
+(defface dead-eye-jump-game-target-face-middle '((t :background "yellow"))
   "Face for the middle layer of the target.")
-(defface target-face-outer
-  '((t :background "green"))
+(defface dead-eye-jump-game-target-face-outer '((t :background "green"))
   "Face for the outer layer of the target.")
 
-(defvar target-layer-scores '(10 30 50))
-(defvar target-size 7)
-(defvar target-template
+(defvar dead-eye-jump-game--layer-scores '(10 30 50))
+(defvar dead-eye-jump-game--target-size 7)
+(defvar dead-eye-jump-game-target-template
   (list
-   (list 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer    'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer    'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-middle 'target-face-middle 'target-face-middle 'target-face-outer 'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-middle 'target-face-center 'target-face-middle 'target-face-outer 'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-middle 'target-face-middle 'target-face-middle 'target-face-outer 'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer    'target-face-outer)
-   (list 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer 'target-face-outer    'target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer    'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer    'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-center 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-middle 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer    'dead-eye-jump-game-target-face-outer)
+   (list 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer 'dead-eye-jump-game-target-face-outer    'dead-eye-jump-game-target-face-outer)
    ))
 
-(defvar current-target-position nil
-  "Store the current target's position as (column line layer).")
-
-(defvar game-score 0
+(defvar dead-eye-jump-game--score 0
   "Initial score of the game.")
 
-(defvar target-game--timer nil)
-(defvar target-game-timer-seconds 60
+(defvar dead-eye-jump-game--timer nil)
+(defvar dead-eye-jump-game-timer-seconds 60
   "Timer for the target game to handle game duration. 0 to endless")
 
-(defun target-game ()
+;;* Internals
+(defun dead-eye-jump-game ()
   "Initialize the target shooting game."
   (interactive)
-  (setq game-score 0)  ; Reset the game score.
-  (let ((buffer (get-buffer-create target-game-buffer)))
+  (setq dead-eye-jump-game--score 0)  ; Reset the game score.
+  (let ((buffer (get-buffer-create dead-eye-jump-game-buffer)))
     (switch-to-buffer buffer)
-    (target-game-mode)
-    (target-game-start-round)
-    (when target-game--timer
-      (cancel-timer target-game--timer))
-    (when (> target-game-timer-seconds 0)
-      (setq target-game--timer (run-with-timer target-game-timer-seconds nil 'target-game-end)))
-    (message "Game initialized. Score: %d" game-score)))
+    (dead-eye-jump-game-mode)
+    (dead-eye-jump-game-start-round)
+    (when dead-eye-jump-game--timer
+      (cancel-timer dead-eye-jump-game--timer))
+    (when (> dead-eye-jump-game-timer-seconds 0)
+      (setq dead-eye-jump-game--timer (run-with-timer dead-eye-jump-game-timer-seconds nil 'dead-eye-jump-game-end)))
+    (add-hook 'post-command-hook 'dead-eye-jump-game-check-hit nil t)
+    (message "Game initialized. Score: %d" dead-eye-jump-game--score)))
 
-;;; TODO not used yet
-(defun target-game-disable ()
-  "Disable the target game mode and clean up."
-  (interactive)
-  (remove-hook 'post-command-hook 'target-game-check-hit t)
-  (kill-buffer target-game-buffer))
-
-(defun target-game-start-round ()
+(defun dead-eye-jump-game-start-round ()
   "Reset the target game."
   (interactive)
-  (target-game-fill-buffer)
+  (dead-eye-jump-game-fill-buffer)
   (move-to-window-line 0)
   (beginning-of-line)
-  (target-game-draw-target)
+  (dead-eye-jump-game-draw-target)
   ;; TODO: почему-то после второго повторения не выбирает мишень
   ;; TODO: why cant use default params?
   ;; (dead-eye-jump 0 0 (frame-pixel-width) (frame-pixel-height) dead-eye-jump-repeats)
   )
 
-(defun target-game-end ()
+(defun dead-eye-jump-game-end ()
   "End the target game and clean up."
   (interactive)
-  (when target-game--timer
-    (cancel-timer target-game--timer)
-    (setq target-game--timer nil))
+  (when dead-eye-jump-game--timer
+    (cancel-timer dead-eye-jump-game--timer)
+    (setq dead-eye-jump-game--timer nil))
   (setq cursor-type nil)
-  (when (get-buffer target-game-buffer)
-    (switch-to-buffer target-game-buffer)
-    (target-game-fill-buffer)
+  (when (get-buffer dead-eye-jump-game-buffer)
+    (switch-to-buffer dead-eye-jump-game-buffer)
+    (dead-eye-jump-game-fill-buffer)
     (move-to-window-line 0)
     (beginning-of-line)
-    (insert (format "Game ended. Final Score: %d" game-score))
+    (insert (format "Game ended. Final Score: %d" dead-eye-jump-game--score))
     (newline)
     (insert-button "Start Game"
-                   'action (lambda (x) (target-game))
+                   'action (lambda (x) (dead-eye-jump-game))
                    'follow-link t
                    'help-echo "Click to start a new game.")
     (beginning-of-line)
     )
-  ;; (kill-buffer target-game-buffer))
-  (message "Game ended. Final Score: %d" game-score))
-;; TODO: remove hook
-;; (remove-hook 'post-command-hook 'target-game-check-hit t))
+  (message "Game ended. Final Score: %d" dead-eye-jump-game--score))
 
-(defun target-game-fill-buffer ()
+(defun dead-eye-jump-game-fill-buffer ()
   "Fill the current buffer with whitespace to match the window height."
   (erase-buffer)  ; Clear the buffer first to ensure it's clean.
   (let ((lines (window-body-height)))  ; Get the number of lines in the current window.
     (dotimes (_ lines)
       (insert (make-string (- (window-body-width) 10) ?\s) "\n"))))
 
-(defun target-game-draw-target ()
+(defun dead-eye-jump-game-draw-target ()
   "Draw a 5x5 target with different colors at a random position in the buffer."
-  (let* ((max-line (- (max 1 (line-number-at-pos (point-max))) target-size))
+  (let* ((max-line (- (max 1 (line-number-at-pos (point-max))) dead-eye-jump-game--target-size))
          (line (random max-line))
-         (max-column (- (max 1 (window-body-width)) target-size 1))
+         (max-column (- (max 1 (window-body-width)) dead-eye-jump-game--target-size 1))
          (column (random max-column))
-         (target-map target-template))
+         (target-map dead-eye-jump-game-target-template))
     (save-excursion
       (goto-char (point-min))
-      (forward-line line)  ; Move down to the random line.
-      (move-to-column column)  ; Move to the random column.
+      (forward-line line)
+      (move-to-column column)
       (let ((start (point)))
-        (dotimes (i target-size)
-          (dotimes (j target-size)
+        (dotimes (i dead-eye-jump-game--target-size)
+          (dotimes (j dead-eye-jump-game--target-size)
             (let ((face (nth j (nth i target-map))))
               (delete-char 1)
               (backward-char 1)
               (insert (propertize " " 'face face)))
             ;; Move to the next character, if not at the end of the row.
-            (when (< j (1- target-size))
+            (when (< j (1- dead-eye-jump-game--target-size))
               (forward-char 1)))
-          ;; After finishing a row, move to the start of the next line.
           (forward-line 1)
           (move-to-column column))
         (overlay-put (make-overlay start (point)) 'target t)))))
 
-(defun target-game-get-remaining-time ()
+(defun dead-eye-jump-game-get-remaining-time ()
   "Get the remaining time in seconds for the target game timer."
-  (if target-game--timer
+  (if dead-eye-jump-game--timer
       (let ((now (current-time))
-            (next-activation (timer--time target-game--timer)))
+            (next-activation (timer--time dead-eye-jump-game--timer)))
         (floor (- (time-to-seconds next-activation) (time-to-seconds now))))
     0))
 
-(defun target-game-check-hit ()
+(defun dead-eye-jump-game-check-hit ()
   "Check if the cursor is on the target and update the score."
-  (let ((face ((if (eobp) nil
+  (let ((face (if (eobp) nil ;; TODO: does it do anything?
                  (get-char-property (point) 'face)))
-        (seconds-left (target-game-get-remaining-time))
+        (seconds-left (dead-eye-jump-game-get-remaining-time))
         points)
     ;; Determine points based on the face at the current point
-    (setq points (cond ((eq face 'target-face-center) (nth 2 target-layer-scores))
-                       ((eq face 'target-face-middle) (nth 1 target-layer-scores))
-                       ((eq face 'target-face-outer) (nth 0 target-layer-scores))
+    (setq points (cond ((eq face 'dead-eye-jump-game-target-face-center) (nth 2 dead-eye-jump-game--layer-scores))
+                       ((eq face 'dead-eye-jump-game-target-face-middle) (nth 1 dead-eye-jump-game--layer-scores))
+                       ((eq face 'dead-eye-jump-game-target-face-outer) (nth 0 dead-eye-jump-game--layer-scores))
                        (t 0)))
     (when (> points 0)
-      (setq game-score (+ game-score points))
+      (setq dead-eye-jump-game--score (+ dead-eye-jump-game--score points))
       (if (> seconds-left 0)
-          (message "Hit! You got %d. Score: %d. Seconds left: %s" points game-score seconds-left)
-        (message "Hit! You got %d. Score: %d" points game-score))
-      (target-game-start-round)
+          (message "Hit! You got %d. Score: %d. Seconds left: %s" points dead-eye-jump-game--score seconds-left)
+        (message "Hit! You got %d. Score: %d" points dead-eye-jump-game--score))
+      (dead-eye-jump-game-start-round)
       (force-mode-line-update))))
 
-(define-derived-mode target-game-mode fundamental-mode "Target-Game"
+(define-derived-mode dead-eye-jump-game-mode fundamental-mode "Dead-Eye-Jump-Game"
   "Major mode for playing the target shooting game."
-  :lighter " Target-Game"
-  ;; (if target-game-mode
-  ;;     (progn
-        (message "Setting up target game mode...")
-        (setq-local cursor-type 'box)
-        (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-        (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-        (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-        (add-hook 'post-command-hook 'target-game-check-hit nil t)
-        )
+  (message "Setting up target game mode...")
+  (setq-local cursor-type 'box)
+  (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+  (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+  (when (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
 
-;; (provide 'target-game)
+(provide 'dead-eye-jump-game)
